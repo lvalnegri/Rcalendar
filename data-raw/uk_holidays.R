a07f7ee34040ff77e8cf6f91ea0a9ba5d49a965c
Luca Valnegri <l.valnegri@datamaps.co.uk>
2023-01-01 23:55:11 +0000

    there were too many holidays!!

--- data-raw/uk_holidays.R
+++ data-raw/uk_holidays.R
@@ -12,6 +12,9 @@ for(yr in 1970:2025){
     yh <- rbindlist(list( yh, y[, .(date, name = V2)]))
     Sys.sleep(2)
 }
+yh <- yh[!grepl('Father|Mother|David|Fawkes|George|Valentine|Halloween', yh$name)]
+yh[name == "New Year's Day" & mday(dte) > 1, name := "Substitute day for New Year's Day"]
+yh <- rbind(yh, data.table(dte = '09-09-2022', name = 'Bank Holiday for the State Funeral of Queen Elizabeth II'))
 yh[, date := as.Date(date, '%d-%m-%Y')]
 setorder(yh, 'dte')
 save_dts_pkg(yh, 'uk_holidays', dbn = 'calendar')
