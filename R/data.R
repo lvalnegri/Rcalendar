6f224d45c8d588a2d914f188a446f2b946575689
Luca Valnegri <l.valnegri@datamaps.co.uk>
2023-01-02 13:17:53 +0000

    added docs, added seasons table

--- R/data.R
+++ R/data.R
@@ -3,38 +3,38 @@ NULL
 
 #' calendar
 #'
-#' Description ...
+#' All days from 1st Jan 1970 until 31st Dec 2025 with some properties, characteristics and formattings.
 #' 
 #' @format A data.table with the following columns:
 #' \describe{
 #'   \item{\code{ dte }}{ the date in *standard* format `YYYY-MM-DD` }
-#'   \item{\code{ is_weekday }}{  }
-#'   \item{\code{ is_workingday }}{  }
-#'   \item{\code{ year }}{  }
-#'   \item{\code{ fyear }}{   }
-#'   \item{\code{ quarter }}{   }
-#'   \item{\code{ qoy }}{  }
-#'   \item{\code{ month }}{  }
-#'   \item{\code{ moy }}{  }
-#'   \item{\code{ moyt }}{  }
-#'   \item{\code{ moyts }}{  }
-#'   \item{\code{ moq }}{  }
-#'   \item{\code{ week }}{  }
-#'   \item{\code{ dweek }}{  }
-#'   \item{\code{ woy }}{  }
-#'   \item{\code{ woq }}{  }
-#'   \item{\code{ wom }}{  }
-#'   \item{\code{ doy }}{  }
-#'   \item{\code{ doq }}{  }
-#'   \item{\code{ dom }}{  }
-#'   \item{\code{ dow }}{ Day of the Week (following ISO-8601 with Monday the first day)  }
-#'   \item{\code{ dowt }}{ Day of the Week as text, an ordered factor with Monday as the first day }
-#'   \item{\code{ dowts  }}{ Day of the Week as short text, an ordered factor with Monday as the first day }
-#'   \item{\code{ rdow }}{ Day of the Week (following most religions with Sunday the first day) }
-#'   \item{\code{ rdowt }}{ Day of the Week as text, an ordered factor with Sunday as the first day }
-#'   \item{\code{ rdowts  }}{ Day of the Week as short text, an ordered factor with Sunday as the first day }
-#'   \item{\code{ mseason }}{ *Meteorological* Season: the starts are the first of the months: March, June, September, December }
-#'   \item{\code{ aseason }}{ *Astronomical* Season: the starts are: 20th March, 21th June, 23th September, 21th December }
+#'   \item{\code{ is_weekday }}{ `Yes` for Mon-Fri, `No` for Sat and Sun }
+#'   \item{\code{ is_workingday }}{ `No` for Sat and Sun plus *holidays* (as listed in the `holidays_uk` table), `Yes` for all the other days }
+#'   \item{\code{ year }}{ Year }
+#'   \item{\code{ fyear }}{ Financial Year (1st April to 31st March)  }
+#'   \item{\code{ quarter }}{ Quarter (as Year + Quarter of the Year) }
+#'   \item{\code{ qoy }}{ Quarter of the Year }
+#'   \item{\code{ month }}{ Month (as Month of the Year in short form text + Year) }
+#'   \item{\code{ moy }}{ Month of the Year }
+#'   \item{\code{ moyt }}{ Month of the Year in text  }
+#'   \item{\code{ moyts }}{ Month of the Year in short form text  }
+#'   \item{\code{ moq }}{ Month of the Quarter }
+#'   \item{\code{ week }}{ Week of the Year (as Year + `-W` + the number of the Week following ISO 8601) }
+#'   \item{\code{ dweek }}{ Date of the Week of the Year (as the standard date of the Monday of the Week of the Year) }
+#'   \item{\code{ woy }}{ Week of the Year (number only) }
+#'   \item{\code{ woq }}{ Week of the Quarter }
+#'   \item{\code{ wom }}{ Week of the Month }
+#'   \item{\code{ doy }}{ Day of the Year }
+#'   \item{\code{ doq }}{ Day of the Quarter }
+#'   \item{\code{ dom }}{ Day of the Month }
+#'   \item{\code{ dow }}{ Day of the Week (following ISO-8601, with Monday the first day of the Week)  }
+#'   \item{\code{ dowt }}{ Day of the Week as text, an ordered factor with Monday as the first day of the Week }
+#'   \item{\code{ dowts  }}{ Day of the Week as short text, an ordered factor with Monday as the first day of the Week }
+#'   \item{\code{ rdow }}{ Gregorian Day of the Week (with Sunday the first day of the Week) }
+#'   \item{\code{ rdowt }}{ Gregorian Day of the Week as text, an ordered factor with Sunday as the first day of the Week }
+#'   \item{\code{ rdowts  }}{ Gregorian Day of the Week as short text, an ordered factor with Sunday as the first day of the Week }
+#'   \item{\code{ mseason }}{ *Meteorological* Season (with the starting days being the first of the months: March, June, September, December) }
+#'   \item{\code{ aseason }}{ *Astronomical* Season (as listed in the `seasons` table) }
 #' }
 #'
 'calendar'
@@ -45,8 +45,25 @@ NULL
 #' 
 #' @format A data.table with the following columns:
 #' \describe{
-#'   \item{\code{ dte }}{ the date }
-#'   \item{\code{ name }}{  the name of the holiday }
+#'   \item{\code{ dte }}{ the date of the holiday in *standard* format `YYYY-MM-DD`}
+#'   \item{\code{ name }}{ the name of the holiday }
 #' }
 #'
 'uk_holidays'
+
+#' Seasons
+#'
+#' Starting Day for the seasons of the year, from 1970 to 2050.
+#' 
+#' Based on the [600-Year NASA/JPL DE405 Ephemeris Model](https://neoprogrammics.com/equinoxes_and_solstices/Equinoxes_and_Solstices_Calculator.php)
+#' 
+#' @format A data.table with the following columns:
+#' \describe{
+#'   \item{\code{ year }}{ the year }
+#'   \item{\code{ spring }}{ the starting day for Spring }
+#'   \item{\code{ summer }}{ the starting day for Summer }
+#'   \item{\code{ autumn }}{ the starting day for Autumn }
+#'   \item{\code{ winter }}{ the starting day for Winter }
+#' }
+#'
+'seasons'
